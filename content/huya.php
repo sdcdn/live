d40eee8bf8bb9a35615557b82922b68af851b14c
sdcdn <82781596+sdcdn@users.noreply.github.com>
2021-04-19 09:59:07 +0800

    Delete huya.php

--- content/huya.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(0);
-$hy=include_once('hy.php');
-$hyid=intval($_GET['hyid']);
-$hyname=$hy[$hyid]['name'];
-?>
-
-<iframe src="https://api.qianqi.net/tv/huya/?id=<?php echo $hy[$hyid]['url']; ?>" width="100%" height="100%" scrolling="no" frameborder="0"></iframe>
-
